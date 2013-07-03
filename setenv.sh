4e422e94c2317091d7f5708334d14b0d9a5e9e08
Josh Birdwell <jbirdwell34@gmail.com>
2013-07-03 13:29:21 -0500

    Add setenv file skelton

---
+++ setenv.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+# run this script as ". ./setenv.sh"
+
+RABBITMQ_URL=""
+REQUEST_QUEUE=""
+
+SERVER_USERNAME=""
+SERVER_PASSWORD=""
+
+SERVER_HOST=""
+SERVER_PORT=
+
+export RABBITMQ_URL
+export REQUEST_QUEUE
+export SERVER_USERNAME
+export SERVER_PASSWORD
+
+export SERVER_HOST
+export SERVER_PORT
